4338f8d6f1cc10334efd68e6f94e5806e9220b2d
Wong Hoi Sing Edison <hswong3i@pantarei-design.com>
2026-02-16 10:12:57 +0800

    alvistack/v0.1.1
        git clean -xdf
        tar zcvf ../python-ansible-pygments_0.1.1.orig.tar.gz --exclude=.git .
        debuild -uc -us
        cp python-ansible-pygments.spec ../python-ansible-pygments_0.1.1-1.spec
        cp ../python*-ansible-pygments*0.1.1*.{gz,xz,spec,dsc} /osc/home\:alvistack/ansible-community-ansible-pygments-0.1.1/
        rm -rf ../python*-ansible-pygments*0.1.1*.*
    
    Signed-off-by: Wong Hoi Sing Edison <hswong3i@pantarei-design.com>

---
+++ setup.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+package_dir = \
+{'': 'src'}
+
+packages = \
+['ansible_pygments']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['pygments>=2.4.0']
+
+entry_points = \
+{'pygments.lexers': ['Ansible-output = '
+                     'ansible_pygments.lexers:AnsibleOutputLexer',
+                     'ansible-output = '
+                     'ansible_pygments.lexers:AnsibleOutputLexer'],
+ 'pygments.styles': ['Ansible = ansible_pygments.styles:AnsibleStyle',
+                     'ansible = ansible_pygments.styles:AnsibleStyle']}
+
+setup_kwargs = {
+    'name': 'ansible-pygments',
+    'version': '0.1.1',
+    'description': 'Tools for building the Ansible Distribution',
+    'author': 'Felix Fontein',
+    'author_email': 'felix@fontein.de',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/ansible-community/ansible-pygments',
+    'package_dir': package_dir,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.6.0,<4.0.0',
+}
+
+
+setup(**setup_kwargs)
